acfc735e08654f46a1783ad87e2e84abde4fd09b
vishwender <vishwender.baloria29@gmail.com>
2026-09-14 15:36:34 +0530

    updating twitter logo

--- resources/views/partials/public-footer.blade.php
+++ resources/views/partials/public-footer.blade.php
@@ -28,7 +28,7 @@
                         ],
                         'X' => [
                             'url' => $siteSocial['x'] ?? null,
-                            'icon' => '<path d="m4 4 16 16M20 4 4 20"/>',
+                            'icon' => '<path fill="currentColor" stroke="none" d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.64 7.584H.47l8.6-9.835L0 1.154h7.594l5.243 6.932 6.064-6.933Zm-1.29 19.49h2.039L6.487 3.24H4.3l13.31 17.403Z"/>',
                         ],
                         'Instagram' => [
                             'url' => $siteSocial['instagram'] ?? null,
